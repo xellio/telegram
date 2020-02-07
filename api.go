3e8f7960fe68541d2eee7c195c610bd0ebae76bf
Richard Prillwitz <richard@prillwitz.io>
2020-02-07 13:02:49 +0100

    cleanup

--- api.go
+++ api.go
@@ -1,6 +1,13 @@
 package telegram
 
-import "net/http"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+)
 
 // BaseURL ...
 const BaseURL = "https://api.telegram.org/bot%s/%s"
@@ -53,4 +60,76 @@ type Response struct {
 	ErrorCode   int         `json:"error_code"`
 	Description string      `json:"description"`
 	Result      interface{} `json:"result"`
+}
+
+//
+// prepareRequest - ...
+//
+func (b *Bot) prepareRequest(definition APIMethod, payload ...interface{}) (*http.Request, error) {
+	var err error
+	var req *http.Request
+	apiURL := fmt.Sprintf(BaseURL, b.token, definition.Action)
+
+	switch definition.Method {
+	case http.MethodGet:
+		req, err = b.prepareGETRequest(apiURL, payload...)
+		break
+	case http.MethodPost:
+		req, err = b.preparePOSTRequest(apiURL, payload...)
+		break
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	return req, err
+}
+
+//
+// prepareGETRequest - ...
+//
+func (b *Bot) prepareGETRequest(apiURL string, payload ...interface{}) (*http.Request, error) {
+	if len(payload) <= 0 {
+		return http.NewRequest(http.MethodGet, apiURL, nil)
+	}
+
+	if len(payload) > 1 {
+		return nil, fmt.Errorf("Unable to handle more than one payload in a GET call. payload should be a map[string]string")
+	}
+
+	baseURL, err := url.Parse(apiURL)
+	if err != nil {
+		return nil, err
+	}
+
+	queryMap := payload[0].(map[string]string)
+	params := url.Values{}
+	for k, v := range queryMap {
+		params.Add(k, v)
+	}
+
+	baseURL.RawQuery = params.Encode()
+	return http.NewRequest(http.MethodGet, baseURL.String(), nil)
+}
+
+//
+// preparePOSTRequest - ...
+//
+func (b *Bot) preparePOSTRequest(apiURL string, payload ...interface{}) (*http.Request, error) {
+
+	var pl interface{}
+	pl = payload
+	if len(payload) == 1 {
+		pl = payload[0]
+	}
+
+	data, err := json.Marshal(pl)
+	if err != nil {
+		return nil, err
+	}
+	log.Println(string(data))
+	return http.NewRequest(http.MethodPost, apiURL, bytes.NewBuffer(data))
 }
--- bot.go
+++ bot.go
@@ -4,9 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"net/url"
 	"reflect"
 )
 
@@ -35,71 +33,6 @@ func New(token string) (*Bot, error) {
 	b.Username = u.Username
 	b.Name = u.FirstName
 	return b, nil
-}
-
-func (b *Bot) prepareRequest(definition APIMethod, payload ...interface{}) (*http.Request, error) {
-	var err error
-	var req *http.Request
-	apiURL := fmt.Sprintf(BaseURL, b.token, definition.Action)
-
-	switch definition.Method {
-	case http.MethodGet:
-		req, err = b.prepareGETRequest(apiURL, payload...)
-		break
-	case http.MethodPost:
-		req, err = b.preparePOSTRequest(apiURL, payload...)
-		break
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-
-	log.Println(req)
-
-	return req, err
-}
-
-func (b *Bot) prepareGETRequest(apiURL string, payload ...interface{}) (*http.Request, error) {
-	if len(payload) <= 0 {
-		return http.NewRequest(http.MethodGet, apiURL, nil)
-	}
-
-	if len(payload) > 1 {
-		return nil, fmt.Errorf("Unable to handle more than one payload in a GET call. payload should be a map[string]string")
-	}
-
-	baseURL, err := url.Parse(apiURL)
-	if err != nil {
-		return nil, err
-	}
-
-	queryMap := payload[0].(map[string]string)
-	params := url.Values{}
-	for k, v := range queryMap {
-		params.Add(k, v)
-	}
-
-	baseURL.RawQuery = params.Encode()
-	return http.NewRequest(http.MethodGet, baseURL.String(), nil)
-}
-
-func (b *Bot) preparePOSTRequest(apiURL string, payload ...interface{}) (*http.Request, error) {
-
-	var pl interface{}
-	pl = payload
-	if len(payload) == 1 {
-		pl = payload[0]
-	}
-
-	data, err := json.Marshal(pl)
-	if err != nil {
-		return nil, err
-	}
-	log.Println(string(data))
-	return http.NewRequest(http.MethodPost, apiURL, bytes.NewBuffer(data))
 }
 
 //
@@ -131,15 +64,21 @@ func (b *Bot) call(action string, payload ...interface{}) (interface{}, error) {
 	if !res.OK {
 		return nil, fmt.Errorf("%d: %s", res.ErrorCode, res.Description)
 	}
+	return prepareResult(res.Result, definition.Result)
 
-	tmp, err := json.Marshal(res.Result)
+}
+
+//
+// prepareResult - ...
+//
+func prepareResult(result interface{}, resultDefinition interface{}) (interface{}, error) {
+	tmp, err := json.Marshal(result)
 	if err != nil {
 		return nil, err
 	}
-
 	decoder := json.NewDecoder(bytes.NewReader(tmp))
-	retValue := reflect.New(reflect.TypeOf(definition.Result))
-	retInner := retValue.Interface()
-	decoder.Decode(retInner)
-	return retInner, err
+	retValue := reflect.New(reflect.TypeOf(resultDefinition))
+	res := retValue.Interface()
+	decoder.Decode(res)
+	return res, err
 }
